6f970ad8cccc1dbeb59d33b11d73dcda2d405da8
Mohamed Ali AMOR <amormedali92@gmail.com>
2021-03-23 11:19:54 +0100

    timer added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import Profile from './Component/profile/Profile';
 import './App.css';
 class App extends Component {
+  state = {
+    show: false,
+  }
+  handleClick = () =>{
+    this.setState({show: !this.state.show})
+    }
   render(){
     return (
       <div className="App">
-        <Profile />
+        {this.state.show? <Profile /> : null}
+        { <button className="btn" onClick={this.handleClick}>{this.state.show ? "Hide Profile" : "Show Profile"}</button> }
       </div>
     );
   }
--- src/Component/profile/Profile.js
+++ src/Component/profile/Profile.js
@@ -1,37 +1,55 @@
 import React, { Component } from "react";
 import './profile.css'
 
-class Profile extends Component{
-    constructor(props){
-        super(props)
-        this.state={
+class Profile extends Component {
+    constructor() {
+        super()
+        this.state = {
             fullName: "Mohame Ali Amor",
             bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
             profession: "Full Stack Developer",
             imgSrc: "images/profilePic.jpg",
             show: false,
+            counter: 0,
         }
-        this.handleClick = () =>{
-        this.setState({show: !this.state.show})
-        };
+        // this.handleClick = () =>{
+        // this.setState({show: !this.state.show})
+        // }
+
+
     }
-    render(){
-        return(
+    // tick() {
+    //     this.setState({
+    //       date: new Date()
+    //     });
+    //   }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.setState({ counter: this.state.counter + 1 }), 1000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+
+    render() {
+        return (
             <div className="outer-container">
-            {
-                this.state.show? <div className="container">
-                <div className="profil-pic">
-                    <img src={this.state.imgSrc} alt="" />
+                {/* {this.state.show? */}
+                <div className="container">
+                    <div className="profil-pic">
+                        <img src={this.state.imgSrc} alt="" />
+                    </div>
+                    <div className="name">
+                        <h1>{this.state.fullName} </h1>
+                        <h3>{this.state.profession}</h3>
+                        <p>{this.state.bio}</p>
+                    </div>
                 </div>
-                <div className="name">
-                    <h1>{this.state.fullName} </h1>
-                    <h3>{this.state.profession}</h3>
-                    <p>{this.state.bio}</p>
-                </div>
-            </div> 
-            : null
-            }
-            <button className="btn" onClick={this.handleClick}>{this.state.show ? "Show Profile": "Hide Profile"}</button>
+                {/* : null
+            } */}
+                {/* <button className="btn" onClick={this.handleClick}>{this.state.show ? "Hide Profile" : "Show Profile"}</button> */}
+                <p>{this.state.counter} seconds</p>
             </div>
         )
     }
